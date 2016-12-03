46d465299d6cbdc97b588fbae99869992d04ad74
Seokseong Jeon <songsong@monet.postech.ac.kr>
2016-12-04 01:24:24 +0900

    add error handling for reset_inject.sh

--- reset_inject.sh
+++ reset_inject.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/sudo /bin/bash
 
-PHY=phy0
-IFACE=wlp1s0
+if [ $# -ne 3 ]; then
+    echo "Usage: ./reset_inject.sh <Wiphy> <monitor_iface> <interface>"
+    exit
+fi
 
-echo "Deactivating monitor interface"
-ip link set mon0 down
-echo "Deleting monitor interface"
-iw dev mon0 del
-echo "Creating managed interface"
-iw phy $PHY interface add $IFACE type managed
+PHY=$1
+MONITOR=$2
+IFACE=$3
+
+ip link show $MONITOR >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    ip link show $MONITOR | grep -i down >/dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Deactivating monitor interface"
+        ip link set $MONITOR down
+    fi
+
+    iw dev $MONITOR info >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Deleting monitor interface"
+        iw dev $MONITOR del
+    fi
+fi
+
+ip link show $IFACE >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Creating managed interface"
+    iw phy $PHY interface add $IFACE type managed
+fi
 
 echo "Reset done"
 
