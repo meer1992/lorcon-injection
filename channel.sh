96cc9641bf999b8571ba0e18460d99d223b02f8d
Seokseong Jeon <songsong@monet.postech.ac.kr>
2016-12-05 00:23:08 +0900

    fix kernel versino aware channel.sh

--- channel.sh
+++ channel.sh
@@ -1,20 +1,29 @@
-freq=$(iw dev mon0 info | sed -n -r 's/.*channel ([0-9]{1,3}).*/\1/p')
-echo $freq
+IFACE=$1
 
-#freq=$(iwconfig mon0 | sed -n -r 's/.*(2\.[0-9]{3}).*/\1/p')
-#if [ -z "$freq" ];
-#then
-#    echo -1
-#    exit
-#fi
-#freqMHz=$(echo "1000*$freq" | bc)
+freq=$(iw dev $IFACE info | sed -n -r 's/.*channel ([0-9]{1,3}).*/\1/p')
 
-#band24=$(echo "$freqMHz<2500" | bc)
-#band50=$(echo "$freqMHz>5000" | bc)
-#if [ "$band24" -eq 1 ]; then
-#    echo "($freqMHz - 2407)/5" | bc
-#fi
-#if [ "$band50" -eq 1 ]; then
-#    echo "34 + ($freqMHz - 5170)/5" | bc
-#fi
+# if `iw dev $IFACE info` returns channel number
+# (newer kernel version)
+if [ -n "$freq" ]; then
+    echo $freq
+    exit
+fi
+
+# (older kernel version)
+freq=$(iwconfig $IFACE | sed -n -r 's/.*(2\.[0-9]{3}).*/\1/p')
+if [ -z "$freq" ];
+then
+    echo -1
+    exit
+fi
+freqMHz=$(echo "1000*$freq" | bc)
+
+band24=$(echo "$freqMHz<2500" | bc)
+band50=$(echo "$freqMHz>5000" | bc)
+if [ "$band24" -eq 1 ]; then
+    echo "($freqMHz - 2407)/5" | bc
+fi
+if [ "$band50" -eq 1 ]; then
+    echo "34 + ($freqMHz - 5170)/5" | bc
+fi
 
